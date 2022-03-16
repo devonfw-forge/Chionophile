b7bff6981ee9b232f54bca0f799bc25309f015cf
airherna <101186485+airherna@users.noreply.github.com>
2022-03-16 12:39:32 +0100

    Generic db

--- postgres/postgres/init/init_db_user.sql
+++ postgres/postgres/init/init_db_user.sql
@@ -1,5 +1,3 @@
 CREATE USER jtq_user WITH PASSWORD 'admin';
 CREATE DATABASE jtq_db WITH OWNER jtq_user;
 
-CREATE USER jtq_user WITH PASSWORD 'admin';
-CREATE DATABASE jtq_db WITH OWNER rust_jtq_user;
