5d79912e7ab46c92bba465af560fd9f3887f9042
airherna <airam.hernandez-rocha-external@capgemini.com>
2022-04-04 11:58:36 +0200

    Updated .gitignore and removed individual benchmark

--- benchmark/src/models/mod.rs
+++
@@ -1,7 +0,0 @@
-pub mod visitor_eto;
-pub mod accesscode_eto;
-pub mod accesscode_post_data;
-pub mod pageable;
-pub mod sort;
-pub mod visitor_search_criteria;
-pub mod queue_eto;
--- benchmark/src/models/pageable.rs
+++
@@ -1,9 +0,0 @@
-use serde::{Serialize, Deserialize};
-use crate::models::sort::Sort;
-
-#[derive(Deserialize, Serialize)]
-pub struct Pageable {
-    pub pageSize: i32,
-    pub pageNumber: i32,
-    pub sort: Option<Vec<Sort>>
-}
