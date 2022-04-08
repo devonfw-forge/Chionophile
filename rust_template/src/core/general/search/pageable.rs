0ea4ff752b4391ba93ec9cd6930ca83b1a884717
airherna <airam.hernandez-rocha-external@capgemini.com>
2022-04-08 09:28:54 +0200

    Removed unused sort property

--- rust_template/src/core/general/search/pageable.rs
+++ rust_template/src/core/general/search/pageable.rs
@@ -1,12 +1,10 @@
 use serde::{Serialize, Deserialize};
-use crate::core::general::search::sort::Sort;
 
 #[derive(Deserialize, Serialize, Clone)]
 #[serde(rename_all="camelCase")]
 pub struct Pageable {
     pub page_size: i32,
     pub page_number: i32,
-    pub sort: Option<Vec<Sort>>
 }
 
 impl Pageable {
