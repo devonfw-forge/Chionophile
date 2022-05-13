00cea9f1766f10477f8e2bdc6e938c15b52201a0
maxhammer-cap <104754651+maxhammer-cap@users.noreply.github.com>
2022-05-13 11:21:42 +0200

    add docker-compose

--- deployment/ec2_setup/setup.sh
+++ deployment/ec2_setup/setup.sh
@@ -11,3 +11,8 @@ git clone https://github.com/devonfw-forge/Chionophile.git
 sudo yum -y install postgresql-devel
 
 sudo yum -y install openssl-devel
+
+sudo curl -L "https://github.com/docker/compose/releases/download/v2.5.0/docker-compose-$(uname -s)-$(uname -m)" -o /bin
+
+sudo chmod +x /usr/local/bin/docker-compose
+
