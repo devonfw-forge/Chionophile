61ec8341d14c36c50b6f2195b649742134b43a7e
airherna <airam.hernandez-rocha-external@capgemini.com>
2022-06-03 12:48:47 +0200

    very initial structure for visitor service

--- wasm/rust/src/lib.rs
+++ wasm/rust/src/lib.rs
@@ -1,15 +1,37 @@
 use anyhow::Result;
+use http::Method;
 use spin_sdk::{
     http::{Request, Response},
     http_component,
 };
 
-/// A simple Spin HTTP component.
 #[http_component]
-fn hello_world(req: Request) -> Result<Response> {
+fn hello(req: Request) -> Result<Response> {
     println!("{:?}", req.headers());
-    Ok(http::Response::builder()
-        .status(200)
-        //.header("foo", "bar")
-        .body(Some("Hello, World!\n".into()))?)
+    match req.method() {
+        &Method::POST => {
+            Ok(http::Response::builder()
+                .status(200)
+                //.header("foo", "bar")
+                .body(Some("POST!\n".into()))?)
+        }
+        &Method::GET => {
+            Ok(http::Response::builder()
+                .status(200)
+                //.header("foo", "bar")
+                .body(Some("GET!\n".into()))?)
+        }
+        &Method::DELETE => {
+            Ok(http::Response::builder()
+                .status(200)
+                //.header("foo", "bar")
+                .body(Some("DELETE!\n".into()))?)
+        }
+    }
+
+    Ok(bad_request())
+}
+
+fn bad_request() -> Response {
+    http::Response::builder().status(400).into()
 }
