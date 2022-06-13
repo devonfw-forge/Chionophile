3fa9e65d4d7e4b5a47ece8faceae8f5b6640aa07
econesag <eduard.conesa-guerrero@capgemini.com>
2022-06-13 10:47:42 +0200

    Initializing parsing

--- wasm/rust/src/visitor/router.rs
+++ wasm/rust/src/visitor/router.rs
@@ -3,6 +3,19 @@ use http::Method;
 use spin_sdk::{
     http::{Request, Response},
 };
+use serde::{Deserialize, Serialize};
+use serde_json::Result as Res;
+
+#[derive(Serialize, Deserialize)]
+struct VisitorEntity {
+    username: String,
+    name: String,
+    password: String,
+    phoneNumber: String,
+    acceptedCommercial: bool,
+    acceptedTerms: bool,
+    userType: bool
+}
 
 pub fn route(req: &Request, parsed_url: Vec<&str>)-> Result<Response>{
     match req.method() {
@@ -12,6 +25,9 @@ pub fn route(req: &Request, parsed_url: Vec<&str>)-> Result<Response>{
                 .status(200)
                 .body(Some("Searching Visitor POST".into()))?)
             }
+            let body_cloned = req.clone();
+            let (_, body) = body_cloned.into_parts();
+            let _body: Res<VisitorEntity> = serde_json::from_slice(&body.unwrap());
             return Ok(http::Response::builder()
                 .status(200)
                 .body(Some(format!("POST Visitor at {} {}",req.uri(), parsed_url[4]).into()))?)
