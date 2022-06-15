7603e3f5313b890727e6389bea14bfc75cbe7061
airherna <airam.hernandez-rocha-external@capgemini.com>
2022-06-15 17:15:28 +0200

    Service should return results

--- wasm/rust_atmos/jtq/src/common/logic/service.rs
+++ wasm/rust_atmos/jtq/src/common/logic/service.rs
@@ -1,14 +1,15 @@
 use crate::common::logic::entity_eto::EntityETO;
 use crate::common::search::criteria::Criteria;
 use crate::common::search::search_result::SearchResult;
+use anyhow::Result;
 
 pub trait Service<ETO, SC, ID>
     where
         ETO: EntityETO,
         SC: Criteria
 {
-    fn get_by_id(id: ID) -> Option<ETO>;
-    fn search(search_criteria: SC) -> SearchResult<ETO>;
-    fn delete(id: ID) -> ID;
-    fn create(eto: ETO) -> ETO;
+    fn get_by_id(id: ID) -> Result<Option<ETO>>;
+    fn search(search_criteria: SC) -> Result<SearchResult<ETO>>;
+    fn delete(id: ID) -> Result<ID>;
+    fn create(eto: ETO) -> Result<ETO>;
 }
