5709977a26c521fe42c7ec55710716612a6259b6
capf4bbo <101186527+capf4bbo@users.noreply.github.com>
2022-06-15 16:34:03 +0200

    GetVisitorId

--- wasm/rust_atmos/GetVisitor/src/lib.rs
+++ wasm/rust_atmos/GetVisitor/src/lib.rs
@@ -2,28 +2,24 @@ use serde::Serialize;
 use suborbital::req;
 use suborbital::runnable::*;
 use jtq::visitor::logic::api::visitor_eto::VisitorEto;
+use suborbital::db;
+use suborbital::db::query;
 
 struct GetVisitor{}
 
 impl Runnable for GetVisitor {
     fn run(&self, input: Vec<u8>) -> Result<Vec<u8>, RunErr> {
-        let in_string = String::from_utf8(input).unwrap();
-
+        suborbital::resp::content_type("application/json; charset=utf-8");
         let id = req::url_param("id");
-        let visitor: VisitorEto = VisitorEto {
-            id: None,
-            modification_counter: None,
-            username: None,
-            name: None,
-            phone_number: None,
-            password: None,
-            accepted_commercial: None,
-            accepted_terms: false,
-            user_type: None
-        };
-        let res = serde_json::to_string(&visitor).unwrap();
+        let mut query_args: Vec<query::QueryArg> = Vec::new();
+        query_args.push(query::QueryArg::new("id", id.as_str()));
 
-        Ok(res.as_bytes().to_vec())
+        match db::select("SelectVisitor", query_args) {
+            Ok(result) => Ok(result),
+            Err(e) => {
+                Err(RunErr::new(500, e.message.as_str()))
+            }
+        }
     }
 }
 
