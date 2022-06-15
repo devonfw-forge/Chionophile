bf084f411e77604cd0825911cbf76ae24a391acf
airherna <airam.hernandez-rocha-external@capgemini.com>
2022-06-15 17:10:22 +0200

    Get By Id returns option

--- wasm/rust_atmos/jtq/src/common/logic/service.rs
+++ wasm/rust_atmos/jtq/src/common/logic/service.rs
@@ -7,7 +7,7 @@ pub trait Service<ETO, SC, ID>
         ETO: EntityETO,
         SC: Criteria
 {
-    fn get_by_id(id: ID) -> ETO;
+    fn get_by_id(id: ID) -> Option<ETO>;
     fn search(search_criteria: SC) -> SearchResult<ETO>;
     fn delete(id: ID) -> ID;
     fn create(eto: ETO) -> ETO;
