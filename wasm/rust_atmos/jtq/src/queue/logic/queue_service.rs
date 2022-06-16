6bfe374118de65777669555a7c2e0a18c27f9a79
capf4bbo <101186527+capf4bbo@users.noreply.github.com>
2022-06-16 13:00:32 +0200

    DeleteQueue finished

--- wasm/rust_atmos/jtq/src/queue/logic/queue_service.rs
+++ wasm/rust_atmos/jtq/src/queue/logic/queue_service.rs
@@ -1,7 +1,6 @@
 use crate::common::logic::service::Service;
 use crate::common::search::pageable::Pageable;
 use crate::common::search::search_result::SearchResult;
-use crate::queue::logic::api::queue_eto::QueueEto;
 use crate::queue::logic::api::queue_search_criteria::QueueSearchCriteria;
 use suborbital::db;
 use suborbital::db::query;
@@ -15,7 +14,7 @@ impl Service<Vec<u8>, QueueSearchCriteria, i64> for QueueService {
         let mut query_args: Vec<query::QueryArg> = Vec::new();
         query_args.push(query::QueryArg::new("id", &id.to_string()));
 
-        let queue_query_result = db::select("SelectVisitor", query_args);
+        let queue_query_result = db::select("SelectQueue", query_args);
         return match queue_query_result {
             Ok(query_res) => {
                 if query_res.len() > 2 {
@@ -41,7 +40,24 @@ impl Service<Vec<u8>, QueueSearchCriteria, i64> for QueueService {
     }
 
     fn delete(id: i64) -> Result<i64> {
-        Ok(id)
+        let mut query_args1: Vec<query::QueryArg> = Vec::new();
+        let mut query_args2: Vec<query::QueryArg> = Vec::new();
+        query_args1.push(query::QueryArg::new("id", &id.to_string()));
+        query_args2.push(query::QueryArg::new("id", &id.to_string()));
+
+        let queue_query_1_result = db::delete("DeleteACQueue", query_args1);
+        let queue_query_2_result = db::delete("DeleteQueue", query_args2);
+        return match queue_query_1_result {
+            Ok(query_res) => {
+                return match queue_query_2_result {
+                    Ok(query_res) => {
+                        Ok(id)
+                    }
+                    Err(e) => Err(anyhow::Error::msg(e.message))
+                }
+            }
+            Err(e) => Err(anyhow::Error::msg(e.message))
+        }
     }
 
     fn create(eto: Vec<u8>) -> Result<Vec<u8>> {
