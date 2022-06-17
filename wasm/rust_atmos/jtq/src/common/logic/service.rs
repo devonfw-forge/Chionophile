c83558f5f5ff0c1949d67e44621973948b6434dd
airam <airam.hernandez-rocha-external@capgemini.com>
2022-06-17 11:08:24 +0200

    Search should return a result

--- wasm/rust_atmos/jtq/src/common/logic/service.rs
+++ wasm/rust_atmos/jtq/src/common/logic/service.rs
@@ -8,7 +8,7 @@ pub trait Service<SC, ID>
         SC: Criteria
 {
     fn get_by_id(id: ID) -> Result<Option<Vec<u8>>>;
-    fn search(search_criteria: SC) -> Vec<u8>;
+    fn search(search_criteria: SC) -> Result<Vec<u8>>;
     fn delete(id: ID) -> Result<ID>;
     fn create(eto: Vec<u8>) -> Result<Vec<u8>>;
 }
