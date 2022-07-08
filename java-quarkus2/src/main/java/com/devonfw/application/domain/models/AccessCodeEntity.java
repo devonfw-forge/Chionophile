6f660693bccc0165480b514a6bcdbfda22e6e7b2
Matus Majchrak <matus.majchrak@capgemini.com>
2022-07-08 09:24:02 +0200

    after merge conflicts

--- java-quarkus2/src/main/java/com/devonfw/application/domain/models/AccessCodeEntity.java
+++ java-quarkus2/src/main/java/com/devonfw/application/domain/models/AccessCodeEntity.java
@@ -2,16 +2,7 @@ package com.devonfw.application.domain.models;
 
 import java.util.Date;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.NamedAttributeNode;
-import javax.persistence.NamedEntityGraph;
-import javax.persistence.OneToOne;
-import javax.persistence.Table;
-import javax.persistence.Transient;
+import javax.persistence.*;
 
 import lombok.Getter;
 import lombok.Setter;
--- java-quarkus2/src/main/java/com/devonfw/application/domain/models/QueueEntity.java
+++ java-quarkus2/src/main/java/com/devonfw/application/domain/models/QueueEntity.java
@@ -4,6 +4,8 @@ import java.util.Date;
 
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 import lombok.Getter;
 import lombok.Setter;
